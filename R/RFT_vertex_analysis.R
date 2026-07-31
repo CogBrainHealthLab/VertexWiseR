9f3a8532627ac009c499914852ca73cb06e38cdd
chabld <charly.billaud@ntu.edu.sg>
2026-07-31 18:16:29 +0800

    Since SubCortexMesh v1.1.0, some fsaverage ROIs have also atlas subparcellations. They can be selected as descriptive region labels in the outputs of RFT and TFCE model, as well as in the hovering information in plotsurf_3d() (atlas=1 for default base ROI, atlas=2 for the anatomical atlas label). This works with the thalamus, hippocampus, amygdala, ventral DC, pallidum, cerebellum and brain-stem (in fsaverage space only).

--- R/RFT_vertex_analysis.R
+++ R/RFT_vertex_analysis.R
@@ -26,7 +26,7 @@
 #' @param inverse A boolean object stating whether to set the surface data as predictor of the contrast variable, instead of as dependent variable (default is FALSE). Other covariates in the model remain independent variables. This makes modelling slower.
 #' @param surf_data A N x V matrix object containing the surface data (N row for each subject, V for each vertex), in fsaverage5 (20484 vertices), fsaverage6 (81924 vertices), fslr32k (64984 vertices) or hippocampal (14524 vertices) space. See also Hipvextract(), SURFvextract() or FSLRvextract output formats. Alternatively, a string object containing the path to the surface object (.rds file) outputted by extraction functions may be given.
 #' @param p A numeric object specifying the p-value to threshold the results (Default is 0.05)
-#' @param atlas A numeric integer object corresponding to the atlas of interest.  1=Desikan, 2=Destrieux-148, 3=Glasser-360, 4=Schaefer-100, 5=Schaefer-200, 6=Schaefer-400. Set to `1` by default. This argument is ignored for hippocampal surfaces.
+#' @param atlas A numeric integer object corresponding to the atlas of interest.  1=Desikan, 2=Destrieux-148, 3=Glasser-360, 4=Schaefer-100, 5=Schaefer-200, 6=Schaefer-400. Set to `1` by default. This argument is ignored for hippocampal surfaces. For applicable SubCortexMesh surfaces, 1=default base ROI, 2=anatomical atlas.
 #' @param smooth_FWHM A numeric vector object specifying the desired smoothing width in mm. It should not be specified if the surf_data has been smoothed previously with smooth_surf(), because this result in surf_data being smoothed twice.
 #' @param VWR_check A boolean object specifying whether to check and validate system requirements. Default is TRUE.
 #'
